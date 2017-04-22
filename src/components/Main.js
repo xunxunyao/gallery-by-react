35f5231a763f378991be89a31ac3266aa7ccba3c
yaoxunxun <xunxunyao@gmail.com>
2017-04-22 17:35:24 +0800

    图片基本样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,20 +8,46 @@ let imageDatas = require('json!../data/imageData.json');
 
 //利用webpack url-loader,只需要传入文件名，即可生成图片的url地址
 //获得图片URL
-imageDatas = (function (imgs) {
+imageDatas = (function(imgs){
   for (let value of imgs) {
     value.imageURL = require('../images/' + value.fileName)
   }
-  return imgs
+  return imgs;
 })(imageDatas);
 
+class ImageFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+              alt={this.props.data.title}/>
+        <figcaption className="img-title">
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+//舞台 大管家的角色
 class GalleryByReactApp extends React.Component {
   render() {
+
+    let controllerUnit = [],
+      imgFigures = [];
+
+    imageDatas.forEach(value=>{
+      imgFigures.push(<ImageFigure data={value}/>)
+    });
+
     return (
       <section className="stage">
         <section className="image-sec">
+          {imgFigures}
         </section>
-        <nav className="controller-nav"></nav>
+        <nav className="controller-nav">
+          {controllerUnit}
+        </nav>
       </section>
     );
   }
